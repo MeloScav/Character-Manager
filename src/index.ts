fae81015fd37811d0e34fee0a46822684fb829c0
Méloé Scavezzoni <meloscavezzoni@gmail.com>
2019-09-30 15:57:20 +0200

    basic requet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 const axios = require('axios');
+
+const instance = axios.create({
+    baseURL: "https://character-database.becode.xyz"
+  });
+
+let character = async ()=>{
+    try{
+        const result = await instance.get("/characters");
+        console.log(result.data);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+character();

93c355e15ee2bc31e5b7d0fbc1d0f736309f347e
Méloé Scavezzoni <meloscavezzoni@gmail.com>
2019-10-02 15:26:28 +0200

    update

--- src/characterEdit.ts
+++ src/characterEdit.ts
@@ -34,9 +34,39 @@ inputSubmit.addEventListener("click",async (e)=>{
     let inputName = (<HTMLInputElement>document.getElementById("name")).value;
     let inputShortDescription = (<HTMLInputElement> document.getElementById("shortDescription")).value;
     let inputDescription = (<HTMLInputElement> document.getElementById("description")).value;
- 
-    //let idHero = e.currentTarget;
     
+    let inputImage = (<HTMLInputElement>document.getElementById("image")).files;
+    let preview:any;
+    let reader  = new FileReader();
+    reader.addEventListener("load", async function () {
+        preview = reader.result;
+        preview = preview.split(",");
+        console.log(preview[1]);
+
+        const rawResponse = await fetch('https://character-database.becode.xyz/characters', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: inputName, 
+            shortDescription: inputShortDescription ,
+            description: inputDescription,
+            image: preview[1],
+        })
+    });
+    const content = await rawResponse.json();
+
+    console.log(content);
+
+      }, false);
+
+    if (inputImage) {
+        reader.readAsDataURL(inputImage[0]);
+      }
+
+
     const rawResponse = await fetch('https://character-database.becode.xyz/characters/:id', {
         method: 'PUT',
         headers: {
@@ -47,10 +77,13 @@ inputSubmit.addEventListener("click",async (e)=>{
             name: inputName, 
             shortDescription: inputShortDescription ,
             description: inputDescription,
+            image: preview[1],
         })
     });
     const content = await rawResponse.json();
 
     console.log(content);
+
+    console.log(inputName,inputShortDescription,inputDescription,preview[1])
  
 })

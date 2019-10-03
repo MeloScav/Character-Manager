8624f70e3142e9b7c4df8abbb634707bb407111f
Méloé Scavezzoni <meloscavezzoni@gmail.com>
2019-10-03 10:34:03 +0200

    edit

--- src/CharacterEdit.ts
+++ src/CharacterEdit.ts
@@ -1,34 +1,8 @@
 const axios = require('axios');
 
-import { AllCharacters } from "./allCharacters";
-
 let url_string = window.location.href; // url
 var url = new URL(url_string);
 var id = url.searchParams.get("id"); //recupère id
-
-// Creating a new "AllCharacters" and displaying items in html and console
-let a = async () => {
-    try {
-        const result = await new AllCharacters(axios);
-        let t = async () => {
-            try {
-                const all = await result.getAllCharacters();
-                all.forEach((el: any) => {
-                    console.log(el);
-                })
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
-        t()
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
-a();
-
 
 // input
 let inputSubmit = document.getElementById("submit");
@@ -47,8 +21,8 @@ inputSubmit.addEventListener("click", async () => {
         preview = preview.split(",");
         console.log(preview[1]);
 
-        const rawResponse = await fetch(`https://character-database.becode.xyz/characters`, {
-            method: 'POST',
+        const rawResponse = await fetch('https://character-database.becode.xyz/characters/:id', {
+            method: 'PUT',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
@@ -61,34 +35,11 @@ inputSubmit.addEventListener("click", async () => {
             })
         });
         const content = await rawResponse.json();
-
+    
         console.log(content);
-
-    }, false);
 
     if (inputImage) {
         reader.readAsDataURL(inputImage[0]);
     }
-
-
-    const rawResponse = await fetch(`https://character-database.becode.xyz/characters/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: inputName,
-            shortDescription: inputShortDescription,
-            description: inputDescription,
-            image: preview[1],
-        })
-    });
-    const content = await rawResponse.json();
-
-    console.log(content);
-
-    console.log(inputName, inputShortDescription, inputDescription, preview[1])
-
-
+})
 })
